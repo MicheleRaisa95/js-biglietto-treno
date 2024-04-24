721d1adc0704089610c8c2fda69bab0fe318fa20
MicheleRaisa95 <micheleraisa95@gmail.com>
2024-04-24 15:10:12 +0200

    [UPD] added prompt for age and distance

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 'use strict;'
+
+// prompt per chiedere all'utente quanta distanza vuole percorrere e età
+
+const km = Number(prompt("Inserisci i Km che vuoi percorrere"));
+console.log(km);
+
+const age = Number(prompt('Quanti anni hai?'))
+console.log(age)
